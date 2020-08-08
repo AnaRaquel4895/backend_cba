8725afaa5374509518cedef5c04c5e5be31385dd
Raquel Toribio <anaraquel4895@gmail.com>
2020-08-08 17:30:02 -0400

    Generando base controller

---
+++ app/Http/Controllers/API/BaseController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BaseController extends Controller
+{
+    public function sendResponse($result, $message, $code = 200)
+    {
+        $response = [
+            'success' => true,
+            'data' => $result,
+            'message' => $message
+        ];
+        return response()->json($response, $code);
+    }
+}
