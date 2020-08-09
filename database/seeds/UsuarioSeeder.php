19d338c1d52313e0e726d9565fbfe3329a2b7364
Raquel Toribio <anaraquel4895@gmail.com>
2020-08-09 16:59:34 -0400

    Creando usuario admin

---
+++ database/seeds/UsuarioSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UsuarioSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+            'name' => 'Adiministrador',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin'),
+        ]);
+    }
+}
