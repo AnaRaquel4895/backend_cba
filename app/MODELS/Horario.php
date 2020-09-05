5f0540c4b699b1db49ded276d05fa273a0735932
Ana Raquel <anaraquel4895@gmail.com>
2020-09-05 19:41:06 -0400

    Cambiando nombres de las tablas para horarios, nivel

--- app/MODELS/Horario.php
+++ app/MODELS/Horario.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Horario extends Model
 {
-    protected $table = 'niveles';
+    protected $table = 'horarios';
 
     protected $fillable = [
         'nombre',
